26f1821638465c0c0bb5b4c654338eb92f254b55
Nicholas Feinberg <pleasingfung@gmail.com>
2014-10-15 20:35:17 -0700

    Add Shadow Dragon Armour
    A heavy armour that makes you stealthier than if you were naked.
    (Stlth++++)
    
    AC/EV are based on storm dragon armour.

--- crawl-ref/source/artefact.cc
+++ crawl-ref/source/artefact.cc
@@ -442,6 +442,9 @@ void artefact_desc_properties(const item_def &item,
         case ARM_STORM_DRAGON_ARMOUR:
             ++proprt[ARTP_ELECTRICITY];
             break;
+        case ARM_SHADOW_DRAGON_ARMOUR:
+            proprt[ARTP_STEALTH] += 4;
+            break;
         case ARM_GOLD_DRAGON_ARMOUR:
             ++proprt[ARTP_POISON];
             ++proprt[ARTP_FIRE];
