4b1b79a2eb742374c97cb890bcf9dd9f6f0a1bdc
Nicholas Feinberg <pleasingfung@gmail.com>
2014-11-10 23:04:04 -0800

    Improve gozag shop quality
    Give a 50% chance of GOOD_ITEMs, up from the base 25%.
    
    Possibly this should be 100%, or perhaps there could be some
    finer adjustment to item_level; this is a tentative start.

--- crawl-ref/source/dungeon.cc
+++ crawl-ref/source/dungeon.cc
@@ -5829,8 +5829,12 @@ static void _stock_shop_item(int j, shop_type shop_type_,
         else
         {
             // make an item randomly
-            item_index = items(true, basetype, subtype,
-                               one_chance_in(4) ? MAKE_GOOD_ITEM : item_level);
+            // gozag shop items are better
+            const bool good_item_chance = spec.gozag ? 2 : 4;
+            const int level = one_chance_in(good_item_chance) ?
+                                MAKE_GOOD_ITEM :
+                                item_level;
+            item_index = items(true, basetype, subtype, level);
         }
 
         // Try for a better selection for bookshops.
